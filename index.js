bf36c5d0c0c3b1ab0a6204f3ebcbfe9f8234fd62
LAPTOP-VFBLUQ2U\USER <matthewfelix88@gmail.com>
2023-09-18 01:14:00 +0700

    Added cors for deployment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors")
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://genealogy-tree.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://genealogy-tree.vercel.app'],
     credentials: true
   }))
 
